d1eb3cadf3e3826aba738eb0df5359bd5c86c7a2
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-08-04 16:33:47 +0100

    router

--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,30 @@
-const Nav = () => {
+import { NavLink } from "react-router-dom";
+
+const Nav = ({ search, setSearch }) => {
   return (
-    <div>
-      <h2>Nav</h2>
-    </div>
+    <nav className="Nav">
+      <form className="searchForm" onSubmit={e => e.preventDefault()}>
+        <label htmlFor="search">Search Posts</label>
+        <input
+          type="text"
+          id="search"
+          placeholder="Search Posts"
+          value={search}
+          onChange={e => setSearch(e.target.value)}
+        />
+      </form>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/post">Post</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
