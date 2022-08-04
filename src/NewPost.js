51f5c38dc56872614fbd6963167e3b528088a6c5
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-08-05 00:55:28 +0100

    router

--- src/NewPost.js
+++ src/NewPost.js
@@ -1,8 +1,31 @@
-const NewPost = () => {
+const NewPost = ({
+  handleSubmit,
+  postTitle,
+  setPostTitle,
+  postBody,
+  setPostBody
+}) => {
   return (
-    <div>
-      <h2>NewPost</h2>
-    </div>
+    <main className="NewPost">
+      <form onSubmit={handleSubmit} className="newPostForm">
+        <label htmlFor="postTitle">Title:</label>
+        <input
+          type="text"
+          id="postTitle"
+          required
+          value={postTitle}
+          onChange={e => setPostTitle(e.target.value)}
+        />
+        <label htmlFor="postBody">Post:</label>
+        <textarea
+          type="text"
+          id="postBody"
+          required
+          value={postBody}
+          onChange={e => setPostBody(e.target.value)}
+        />
+      </form>
+    </main>
   );
 };
 
