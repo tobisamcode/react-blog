1769eced3b0a1bf33637ac6c06ee4e5a9a4f9813
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-08-04 23:51:38 +0100

    router

--- src/PostPage.js
+++ src/PostPage.js
@@ -1,8 +1,33 @@
-const PostPage = () => {
+import { useNavigate, useParams } from "react-router-dom";
+
+const PostPage = ({posts, setPosts}) => {
+  const { id } = useParams();
+  const post = posts.find(post => post.id == id)
+
+
+  const navigate = useNavigate()
+
+  const handleDelete =  async(id) => {
+    const remainingPosts = posts.filter(post => post.id != id);
+    setPosts(remainingPosts);
+    navigate("/");
+  };
+
   return (
-    <div>
-      <h2>PostPage</h2>
-    </div>
+    <main className="PostPage">
+      <article className="post">
+        {
+          post && 
+          <>
+            <h2>{post.title}</h2>
+            <p className="postDate">{post.datetime}</p>
+            <p className="postBody">{post.body}</p>
+
+            <button onClick={() => handleDelete(post.id)}> Delete Post </button>
+          </>
+        }
+      </article>
+    </main>
   );
 };
 
