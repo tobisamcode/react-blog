a46a0ae386684265be5bf26d19f8c0bfc90e236b
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-08-04 17:12:13 +0100

    router

--- src/App.js
+++ src/App.js
@@ -1,27 +1,59 @@
 import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import About from "./About";
+import Error from "./Error";
 import Footer from "./Footer";
 import Header from "./Header";
 import Home from "./Home";
-import Missing from "./Missing";
 import Nav from "./Nav";
 import NewPost from "./NewPost";
 import PostPage from "./PostPage";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: "My First Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    },
+    {
+      id: 2,
+      title: "My 2nd Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    },
+    {
+      id: 3,
+      title: "My 3rd Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    },
+    {
+      id: 4,
+      title: "My Fourth Post",
+      datetime: "July 01, 2021 11:17:36 AM",
+      body:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    }
+  ]);
   const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
   return (
     <div className="App">
       <BrowserRouter>
         <Header title="React Js Blog" />
         <Nav search={search} setSearch={setSearch} />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home posts={posts} />} />
           <Route path="post" element={<NewPost />} />
-          <Route path="post/:id" element={<PostPage />} />{" "}
+          <Route path="post/:id" element={<PostPage />} />
           <Route path="about" element={<About />} />
-          <Route path="*" element={<Missing />} />
+          <Route path="*" element={<Error />} />
         </Routes>
       </BrowserRouter>
       <Footer />
