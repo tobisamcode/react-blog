d08ebbc70594a3742efacd10fe4f5d81a7b0ee98
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-08-05 04:32:57 +0100

    router

--- src/Error.js
+++ src/Error.js
@@ -1,3 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Error() {
-  return <div>Error</div>;
+  return (
+    <main className="Missing">
+      <h2>Page Not Found!</h2>
+      <p>Well, that's disappointing</p>
+      <p>
+        <Link to="/">Visit Our Homepage</Link>
+      </p>
+    </main>
+  );
 }
