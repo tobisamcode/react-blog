a9a1feb582e95a5ed64186afd92a1a49deea7ebf
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-08-05 04:20:58 +0100

    router

--- src/NewPost.js
+++ src/NewPost.js
@@ -1,3 +1,6 @@
+import { format } from "date-fns";
+import { useNavigate } from "react-router-dom";
+
 const NewPost = ({
   posts,
   setPosts,
@@ -6,9 +9,20 @@ const NewPost = ({
   postBody,
   setPostBody
 }) => {
-  const handleSubmit = () => {
-    // const id =
+  const navigate = useNavigate();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+    const datetime = format(new Date(), "MMMM dd yyyy pp");
+    const newPost = { id, title: postTitle, datetime, body: postBody };
+    const allPosts = [...posts, newPost];
+    setPosts(allPosts);
+    setPostTitle("");
+    setPostBody("");
+    navigate("/");
   };
+
   return (
     <main className="NewPost">
       <form onSubmit={handleSubmit} className="newPostForm">
