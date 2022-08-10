a2a5818198dbb6e319df3de16140bf659bf84f58
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-08-10 15:33:48 +0100

    axios

--- src/EditPost.js
+++ src/EditPost.js
@@ -1,6 +1,6 @@
 import { format } from "date-fns";
 import { useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import api from "./api/posts";
 
 function EditPost({
@@ -48,27 +48,37 @@ function EditPost({
         {editTitle && 
             <>
                 <h2>Edit Post</h2>
-                <form onSubmit={handleSubmit} className="newPostForm">
+                <form onSubmit={(e) => e.preventDefault()} className="newPostForm">
                     <label htmlFor="postTitle">Title:</label>
                     <input
                         type="text"
                         id="postTitle"
                         required
-                        value={postTitle}
-                        onChange={e => setPostTitle(e.target.value)}
+                        value={editTitle}
+                        onChange={e => setEditTitle(e.target.value)}
                     />
-                    <label htmlFor="postBody">Post:</label>
+                    <label htmlFor="editBody">Post:</label>
                     <textarea
                         type="text"
                         id="postBody"
                         required
-                        value={postBody}
-                        onChange={e => setPostBody(e.target.value)}
+                        value={editBody}
+                        onChange={e => setEditBody(e.target.value)}
                     />
-                    <button type="submit">Submit</button>
+                    <button type="submit" onClick={() => handleEdit(post.id)}>Submit</button>
                 </form>
             </>
-            }
+        }
+        {!editTitle && 
+            <>
+                <h2>Post Not Found</h2>
+                <p>Well, that's dissapointing.</p>
+                <p>
+                    <Link to='/'>Visit Our Homepage</Link>
+                </p>
+            </>
+
+        }
     </main>
     );
 
