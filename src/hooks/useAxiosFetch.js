0158ea53dddc4e25cbc2d9da631ca4ef72cc00df
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-08-11 10:45:50 +0100

    axios

---
+++ src/hooks/useAxiosFetch.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function useAxiosFetch(dataUrl) {
+  const [data, setData] = useState([]);
+  const [fetchError, setFetchError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    let isMounted = true;
+    const source = axios.CancelToken.source(); // cancellation token for axios
+
+    const fetchData = async url => {
+      setIsLoading(true);
+      try {
+        const response = await axios.get(url, {
+          cancelToken: source.token
+        });
+
+        if (isMounted) {
+          setData(response.data);
+          setFetchError(null);
+        }
+      } catch (err) {
+        if (isMounted) {
+          setFetchError(err.message);
+          setData([]);
+        }
+      } finally {
+        isMounted && setTimeout(() => setIsLoading(false), 2000);
+      }
+    };
+
+    fetchData(dataUrl);
+  });
+
+  return { data, fetchError, isLoading };
+}
+
+export default useAxiosFetch;
