527f169136d7492ea09773c4a458383e82df13c0
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-08-11 03:33:01 +0100

    axios

--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -18,13 +18,11 @@ function useWindowSize() {
 
     window.addEventListener("resize", handleResize);
 
-    const cleanUp = () => {
-      console.log("runs if a useEffect dep changes");
-      window.removeEventListener("resize", handleResize);
-    };
-
-    return cleanUp;
+    // clean up
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
+
+  return windowSize;
 }
 
 export default useWindowSize;
