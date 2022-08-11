dedda32cd4fb2db9e678e6f528afcaeb938c7e83
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-08-11 13:53:42 +0100

    axios

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import PostPage from "./PostPage";
 import api from "./api/posts";
 import EditPost from "./EditPost";
 import useWindowSize from "./hooks/useWindowSize";
+import useAxiosFetch from "./hooks/useAxiosFetch";
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -22,25 +23,16 @@ function App() {
   const [editBody, setEditBody] = useState("");
   const { width } = useWindowSize();
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const response = await api.get("/posts");
-        setPosts(response.data);
-      } catch (err) {
-        if (err.response) {
-          // Not in the 200 response range
-          console.log(err.response.data);
-          console.log(err.response.status);
-          console.log(err.response.Headers);
-        } else {
-          console.log(`Error: ${err.message}`);
-        }
-      }
-    };
+  const { data, fetchError, isLoading } = useAxiosFetch(
+    "http://localhost:3500/posts"
+  );
 
-    fetchPosts();
-  }, []);
+  useEffect(
+    () => {
+      setPosts(data);
+    },
+    [data]
+  );
 
   useEffect(
     () => {
@@ -60,7 +52,16 @@ function App() {
         <Header title="React Js Blog" width={width} />
         <Nav search={search} setSearch={setSearch} />
         <Routes>
-          <Route path="/" element={<Home posts={searchResults} />} />
+          <Route
+            path="/"
+            element={
+              <Home
+                posts={searchResults}
+                fetchError={fetchError}
+                isLoading={isLoading}
+              />
+            }
+          />
           <Route
             path="post"
             element={
